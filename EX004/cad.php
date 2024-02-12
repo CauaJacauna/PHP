1446fb1b60cf74b7c03ffa0e155ee6a17f94b783
Cauã Jacaúna <jacaunacaua2018@gmail.com>
2024-02-12 20:28:29 -0300

    Teste com _GET e _REQUEST

---
+++ EX004/cad.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <main>
+        <?php
+        $nome = $_REQUEST['nome'] ?? 'sem nome';
+        $sobrenome = $_REQUEST['snome'] ?? 'desconhecido';
+
+        echo ("É um prazer ter você aqui  $nome  $sobrenome");
+
+       ?>
+    <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>    
+    </main>
+</body>
+</html>
